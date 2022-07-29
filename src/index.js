30b48d0b62a14caf1726195eb9aeb503fd896f10
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-07-29 14:43:04 +0400

    import './index.css';// возьми этот код который в css и запусти

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';// возьми этот код который в css и запусти 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
