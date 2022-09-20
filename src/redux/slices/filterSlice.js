45f363b682674cd8a3b7a15ca47630db2e587db7
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-09-20 15:26:43 +0400

    setCategoryId(state, action){ // при вызове получит стейт и действие

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -2,14 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 //делаю первое со-ие
 const initialState = {
-    categoryId: 0,
-    sort: {
-        name: 'по популярности',
-        sortProperty:'rating',
-    }
-}
+  categoryId: 0,
+  sort: {
+    name: "по популярности",
+    sortProperty: "rating",
+  },
+};
 
 //создаю сам Slice и передаю в него объект
 const filterSlice = createSlice({
+  name: "filter", // название слайся для реакта
+  initialState,
+  // делаю actions(команды)
+  reducers:{ // передаю объект и метод отвечающий за id
+    setCategoryId(state, action){ // при вызове получит стейт и действие
+state.categoryId = 
+    }
+  }
 
-})
+});
