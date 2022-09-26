d38b5cfb72a86735bb1a84e9989dd15001f5c38d
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-09-26 15:44:33 +0400

    cart,// добавила слайс cart

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -13,8 +13,15 @@ export const cartSlice = createSlice({
     addItem(state, action) {
       state.items.push(action.payload);
     },
+    removeItem(state, action) {
+        state.items.filter(obj =>  obj.id !== action.payload);//найти объект у которого id не совпадает с action.payload я payload буду передавать id )
   },
+  clearItems(state,action){
+    state.items = [];
+    state.totalPrice = 0;//очищаю тоталпрайс
+  }
+}
 });
 
-export const {addItem}= cartSlice.actions;
+export const {addItem, removeItem, clearItems }= cartSlice.actions;
 export default cartSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filter from './slices/filterSlice'
-
+import cart from './slices/cart'
 //создала хранилище редакс
 export const store = configureStore({
   reducer: {
     filter,// поместила в хранилище 1 слайс, // логика меняющая состояние
-    
+    cart,// добавила слайс cart
   }
 })
 
